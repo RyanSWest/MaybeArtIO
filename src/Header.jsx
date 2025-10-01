ab08d39fb8540f2664edcb5bb6754e788eb9f0d7
RyanSWest <west56309@gmail.com>
2025-10-01 10:52:57 -0500

    geez

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,10 +6,10 @@ import {Link, Route} from 'react-router-dom';
  export default function  Header() {
   return (
     <div className ='nav'> 
-     <a href='/'>  <span  style ={{padding: '1rem',color: 'white', fontSize: 'x-large', fontFamily:'Rajdhani',fontStyle:'bolder' , fontWeight:'700'}}> Home</span>  </a> 
-     <Link to= '/about'> <span  style ={{color: 'white', fontSize: 'x-large',fontFamily:'Rajdhani' , fontStyle:'bolder',fontWeight:'700' }}> About</span>   </Link>
-     <Link to ='/grid'><span style ={{color: 'white', fontSize: 'x-large' ,fontFamily:'Rajdhani' , fontStyle:'bolder',fontWeight:'700' }}>NFT  Gallery</span>  </Link>
-     <Link to ='/whitepaper'> <span style ={{color: 'white', fontSize: 'x-large' ,fontFamily:'Rajdhani' , fontStyle:'bolder',fontWeight:'700' }}> Whitepaper</span>  </Link>
+     <a href='/'>  <span  style ={{padding: '1rem',color: 'white', fontSize: 'large', fontFamily:'Rajdhani',fontStyle:'bolder' , fontWeight:'700',padding:'.5rem'}}> Home</span>  </a> 
+     <Link to= '/about'> <span  style ={{color: 'white', fontSize: 'large',fontFamily:'Rajdhani' , fontStyle:'bolder',fontWeight:'700',padding:'.5rem' }}> About</span>   </Link>
+     <Link to ='/grid'><span style ={{color: 'white', fontSize: 'large' ,fontFamily:'Rajdhani' , fontStyle:'bolder',fontWeight:'700',padding:'.5rem' }}>NFT  Gallery</span>  </Link>
+     <Link to ='/whitepaper'> <span style ={{color: 'white', fontSize: 'large' ,fontFamily:'Rajdhani' , fontStyle:'bolder',fontWeight:'700' ,padding:'.5rem'}}> Whitepaper</span>  </Link>
         
        <WalletButton  />
        
