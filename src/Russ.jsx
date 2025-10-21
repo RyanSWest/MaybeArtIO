ce8f5a051058e17e8edee6244a8c307cd0e4f9cc
RyanSWest <west56309@gmail.com>
2025-10-21 02:30:46 -0500

    netlify crap

--- src/Russ.jsx
+++ src/Russ.jsx
@@ -1,50 +1,50 @@
-// import {require} from 'react'
-// const images = require.context('./Russ', true);
-// const imageList = images.keys().map(image => images(image))
-import { Container, Image, Row, Col } from "react-bootstrap";
-import "./index.css";
+// // import {require} from 'react'
+// // const images = require.context('./Russ', true);
+// // const imageList = images.keys().map(image => images(image))
+// import { Container, Image, Row, Col } from "react-bootstrap";
+// import "./index.css";
 
-import Russ2 from "./imgs/RUSSL/RUSS2.jpg";
-import Russ3 from "./imgs/RUSSL/RUSS3.jpg";
- import Russ5 from "./imgs/RUSSL/RUSS5.jpg";
-import Russ6 from "./imgs/RUSSL/RUSS6.jpg";
-import "./new.css";
+// import Russ2 from "./imgs/RUSSL/RUSS2.jpg";
+// import Russ3 from "./imgs/RUSSL/RUSS3.jpg";
+//  import Russ5 from "./imgs/RUSSL/RUSS5.jpg";
+// import Russ6 from "./imgs/RUSSL/RUSS6.jpg";
+// import "./new.css";
 
-const Russ = () => {
-  return (
-    <Container>
-      <Row>
+// const Russ = () => {
+//   return (
+//     <Container>
+//       <Row>
         
-        <Col>
-          <div className="crap">
-            <Image src={Russ2} />
-          </div>
+//         <Col>
+//           <div className="crap">
+//             <Image src={Russ2} />
+//           </div>
 
-          <div className="crap">
-            <Image src={Russ3} />
-          </div>
-          <div className="crap">
-           </div>
+//           <div className="crap">
+//             <Image src={Russ3} />
+//           </div>
+//           <div className="crap">
+//            </div>
 
            
-        </Col>
+//         </Col>
 
-        <Col>  
+//         <Col>  
         
-           <div className="crap">
-            <Image src={Russ5} />
-          </div>
+//            <div className="crap">
+//             <Image src={Russ5} />
+//           </div>
 
-          <div className="crap">
-            <Image src={Russ6} />
-          </div>
+//           <div className="crap">
+//             <Image src={Russ6} />
+//           </div>
         
         
         
-        </Col>
+//         </Col>
        
-      </Row>
-    </Container>
-  );
-};
-export default Russ;
+//       </Row>
+//     </Container>
+//   );
+// };
+// export default Russ;
